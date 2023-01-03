8766eaccb9922451ea5dad649852589f9fefbf27
AlexKuzm <kozjavka2000@gmail.com>
2023-01-03 06:59:02 +0100

    added radio button to choose the initial value

--- pages/69_Stable & Unstable_Manifolds.py
+++ pages/69_Stable & Unstable_Manifolds.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
+import mpld3
+import streamlit.components.v1 as components
 from utils.dynamical_systems_general import *
 
 
@@ -31,7 +33,17 @@ Eu_p, = ax_1.plot(*ex1_5_Eu(x_steps), linewidth= lw, label = "Unstable Eigenspac
 Wu_p, = ax_1.plot(*ex1_5_Wu(y_steps), linewidth= lw, label = "Unstable Manifold", color = "blue")
 
 #point = np.array([-2,-2])
-point = ex1_5_Wu(-2)
+
+pts = {
+    "On_Staple": ex1_5_Ws(4),
+    "On_Unstable": ex1_5_Wu(-2),
+    "Neither": np.array([0.2,4])
+}
+
+pt_choice = st.radio("Point",(list(pts.keys())))
+#pt_choice = "On_Staple"
+point = pts[pt_choice]
+#point = ex1_5_Wu(-2)
 point_p, = ax_1.plot(*point,"o")
 
 trajectory = [[point[0]],[point[1]]]
@@ -41,6 +53,8 @@ ax_1.set_xlim([-5.5,5.5])
 ax_1.set_ylim([-5.5,5.5])
 ax_1.legend()
 st_plot = st.pyplot(fig_1)
+#fig_html = mpld3.fig_to_html(fig_1)
+#st_plot = components.html(fig_html, height=600)
 
 def start_pts():
     global point, point_p
@@ -50,5 +64,9 @@ def start_pts():
         st_plot.pyplot(fig_1)
         #time.sleep(0.0000005)
 
-
 st.button(label = "start", key="start_btn", on_click=start_pts, args=None, kwargs=None)
+
+def plot_clicked():
+    st.write("hi")
+
+#st_plot.on_click(plot_clicked)
