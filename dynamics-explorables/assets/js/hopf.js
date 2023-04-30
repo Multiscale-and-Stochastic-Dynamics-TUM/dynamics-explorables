25900058c51ace28a03b34a4afc053c2ccdb96f9
Alejandro Martinez Sanchez <alejandrow40k@gmail.com>
2023-04-30 18:55:26 +0200

    Removed utils and style changes

--- dynamics-explorables/assets/js/hopf.js
+++ dynamics-explorables/assets/js/hopf.js
@@ -1,152 +1,169 @@
-import Plotly from 'plotly.js-dist-min'
-import {getLinspace} from './modules/utils/utils';
+import Plotly from 'plotly.js-dist-min';
 
-//Style constants
-const STABLE_LINE_STYLE = {color: 'blue', width: 5}
-const UNSTABLE_LINE_STYLE = {color: 'blue', width: 5, dash: 'dash'}
-const EIGENVAL_MARKER = {color:"Purple", size: 10}
+import {linspace} from './modules/data_structures/iterables';
+
+// Style constants
+const STABLE_LINE_STYLE = {
+  color: 'blue',
+  width: 5
+};
+
+const UNSTABLE_LINE_STYLE = {
+  color: 'blue',
+  width: 5,
+  dash: 'dash'
+};
+
+const EIGENVAL_MARKER = {
+  color: 'Purple',
+  size: 10
+};
 
 const LAYOUT_3D = {
-    margin: {l: 40, r: 20, t: 20, b: 30},
-    scene: {
-      xaxis: {
-        range: [-4, 4],
-        visible: true,
-        tick0: -50,
-        dtick: 25,
-        zerolinewidth: 2,
-      },
-      yaxis: {
-        range: [-4, 4],
-        visible: true,
-        tick0: -50,
-        dtick: 25,
-        zerolinewidth: 2,
-      },
-      zaxis: {
-        range: [-4, 4],
-        visible: true,
-        tick0: 0,
-        dtick: 25,
-        zerolinewidth: 2,
-      },
-      aspectmode: 'cube',
-      camera: {up: {x: 1, y: 0, z: 0}, center: {x: 0, y: 0, z: 0}, eye: {x: 0.5, y: 2.5, z: 1.0}},
-      dragmode: 'orbit',
+  margin: {l: 40, r: 20, t: 20, b: 30},
+  scene: {
+    xaxis: {
+      range: [-4, 4],
+      visible: true,
+      tick0: -50,
+      dtick: 25,
+      zerolinewidth: 2,
     },
-    modebar: {remove: ['pan3d']},
-    paper_bgcolor: '#ffffff00',
-  };
+    yaxis: {
+      range: [-4, 4],
+      visible: true,
+      tick0: -50,
+      dtick: 25,
+      zerolinewidth: 2,
+    },
+    zaxis: {
+      range: [-4, 4],
+      visible: true,
+      tick0: 0,
+      dtick: 25,
+      zerolinewidth: 2,
+    },
+    aspectmode: 'cube',
+    camera: {
+      up: {x: 1, y: 0, z: 0},
+      center: {x: 0, y: 0, z: 0},
+      eye: {x: 0.5, y: 2.5, z: 1.0}
+    },
+    dragmode: 'orbit',
+  },
+  modebar: {remove: ['pan3d']},
+  paper_bgcolor: '#ffffff00',
+};
 
 // Generate data for the 3D parabola of stability
-function get3Dstability(gridLim=4.0, meshSize=50){
+function get3Dstability(gridLim = 4.0, meshSize = 50) {
   var x = [];
   var y = [];
   var z = [];
 
-  var v =getLinspace(-gridLim, gridLim, meshSize)
-  for (var i = 0; i < meshSize; i++){
-      for (var j = 0; j < meshSize; j++){
-          x.push(v[j]);
-          y.push(v[i]);
-          z.push(v[j] * v[j] + v[i] * v[i])
-      }
+  var v = linspace(-gridLim, gridLim, meshSize)
+  for (var i = 0; i < meshSize; i++) {
+    for (var j = 0; j < meshSize; j++) {
+      x.push(v[j]);
+      y.push(v[i]);
+      z.push(v[j] * v[j] + v[i] * v[i])
+    }
   }
   return [x, y, z];
-}
+};
 
 let plotlyDiv = document.getElementById('plotlyHopf');
 let multiplotDiv = document.getElementById('plotlyMultiplot');
 
 var xyzData = get3Dstability();
 var trace3Dcone = {
-    x: xyzData[0],
-    y: xyzData[1],
-    z: xyzData[2],
-    opacity: 0.8,
-    color:'blue',
-    type: 'mesh3d'
-}
+  x: xyzData[0],
+  y: xyzData[1],
+  z: xyzData[2],
+  opacity: 0.8,
+  color: 'blue',
+  type: 'mesh3d'
+};
 
-//Add all the traces
+// Add all the traces
 var traceStableLineEq = {
-    x: [0, 0], 
-    y: [0, 0],
-    z: [-15, 0], 
-    mode: 'lines',
-    opacity: 0.8,
-    color:'blue',
-    type: 'scatter3d',
-    line: STABLE_LINE_STYLE,
-    showlegend: false
-}
+  x: [0, 0],
+  y: [0, 0],
+  z: [-15, 0],
+  mode: 'lines',
+  opacity: 0.8,
+  color: 'blue',
+  type: 'scatter3d',
+  line: STABLE_LINE_STYLE,
+  showlegend: false
+};
 
 var traceUnstableLineEq = {
-    x: [0, 0], 
-    y: [0, 0],
-    z: [0, 15], 
-    mode: 'lines',
-    opacity: 0.8,
-    color:'blue',
-    type: 'scatter3d',
-    line: UNSTABLE_LINE_STYLE,
-    showlegend: false
-}
+  x: [0, 0],
+  y: [0, 0],
+  z: [0, 15],
+  mode: 'lines',
+  opacity: 0.8,
+  color: 'blue',
+  type: 'scatter3d',
+  line: UNSTABLE_LINE_STYLE,
+  showlegend: false
+};
 
 var traceCuttingPlane = {
-    x: [],
-    y: [],
-    z: [],
-    opacity: 0.2,
-    color:'red',
-    type: 'mesh3d',
-    showlegend: false
-}
+  x: [],
+  y: [],
+  z: [],
+  opacity: 0.2,
+  color: 'red',
+  type: 'mesh3d',
+  showlegend: false
+};
 
-var plotData = [trace3Dcone, traceStableLineEq, traceUnstableLineEq, traceCuttingPlane] 
+var plotData =
+    [trace3Dcone, traceStableLineEq, traceUnstableLineEq, traceCuttingPlane];
 Plotly.newPlot(plotlyDiv, plotData, LAYOUT_3D);
 
 // TODO Implement the two bottom plots
 var traceEigenvals = {
-    x: [-3, -3],
-    y: [1, -1],
-    type: 'scatter',
-    mode: 'markers',
-    marker: EIGENVAL_MARKER,
-    showlegend: false
-  };
-  
-  var trace2 = {
-    x: [20, 30, 40],
-    y: [50, 60, 70],
-    xaxis: 'x2',
-    yaxis: 'y2',
-    type: 'scatter',
-    showlegend: false
-  };
-  
-  var data = [traceEigenvals, trace2];
-  
-  var layout = {
-    grid: {rows: 1, columns: 2, pattern: 'independent'},
-  };
-  
-  Plotly.newPlot(multiplotDiv, data, layout);
+  x: [-3, -3],
+  y: [1, -1],
+  type: 'scatter',
+  mode: 'markers',
+  marker: EIGENVAL_MARKER,
+  showlegend: false
+};
+
+var trace2 = {
+  x: [20, 30, 40],
+  y: [50, 60, 70],
+  xaxis: 'x2',
+  yaxis: 'y2',
+  type: 'scatter',
+  showlegend: false
+};
+
+var data = [traceEigenvals, trace2];
+
+var layout = {
+  grid: {rows: 1, columns: 2, pattern: 'independent'},
+};
+
+Plotly.newPlot(multiplotDiv, data, layout);
 
 const parampSlider = document.getElementById('parampSlider');
 const parampLabel = document.getElementById('parampSliderLabel');
 parampLabel.innerHTML = `p = ${parampSlider.value}`;
 
 parampSlider.oninput = () => {
-    var param_value = parampSlider.value
-    parampLabel.innerHTML = `p = ${param_value}`;
+  var param_value = parampSlider.value;
+  parampLabel.innerHTML = `p = ${param_value}`;
 
-    var xyzDataPlane = get3Dstability(4.0, 2);
-    var updatedStabilityData = {
-        x: [xyzDataPlane[0]],
-        y: [xyzDataPlane[1]],
-        z: [Array(xyzDataPlane[0].length).fill(param_value)]
-    }
-    Plotly.update(plotlyDiv, updatedStabilityData, {}, [3])
-
+  var xyzDataPlane = get3Dstability(4.0, 2);
+  var updatedStabilityData = {
+    x: [xyzDataPlane[0]],
+    y: [xyzDataPlane[1]],
+    z: [Array(xyzDataPlane[0].length).fill(param_value)]
+  };
+  Plotly.update(plotlyDiv, updatedStabilityData, {}, [3]);
 };
--- dynamics-explorables/assets/js/pitchfork.js
+++ dynamics-explorables/assets/js/pitchfork.js
@@ -1,173 +1,198 @@
-import Plotly from 'plotly.js-dist-min'
-import {getLinspace} from './modules/utils/utils';
+import Plotly from 'plotly.js-dist-min';
 
-const X_LIMINF = -2.0
-const x_LIMSUP = 2.0
-const NUM_POINTS = 10000
+import {linspace} from './modules/data_structures/iterables';
+
+const X_LIMINF = -2.0;
+const x_LIMSUP = 2.0;
+const NUM_POINTS = 10000;
 
 const LAYOUT_PITCHFORK = {
-    margin: {l: 40, r: 20, t: 20, b: 30},
-    xaxis: {title: 'x', range: [-2, 2],},
-    yaxis: {title: 'y', range: [-2, 2], },
-    modebar: {remove: ['pan3d', 'resetCameraDefault3d']},
-    paper_bgcolor: '#ffffff00',
+  margin: {l: 40, r: 20, t: 20, b: 30},
+  xaxis: {
+    title: 'x',
+    range: [-2, 2],
+  },
+  yaxis: {
+    title: 'y',
+    range: [-2, 2],
+  },
+  modebar: {remove: ['pan3d', 'resetCameraDefault3d']},
+  paper_bgcolor: '#ffffff00',
 };
 
 const LAYOUT_STABILITY = {
-    margin: {l: 40, r: 20, t: 20, b: 30},
-    xaxis: {title: 'x', range: [-2, 2],},
-    yaxis: {title: 'y', range: [-0.5, 0.5],},
-    modebar: {remove: ['pan3d', 'resetCameraDefault3d']},
-    paper_bgcolor: '#ffffff00',
+  margin: {l: 40, r: 20, t: 20, b: 30},
+  xaxis: {
+    title: 'x',
+    range: [-2, 2],
+  },
+  yaxis: {
+    title: 'y',
+    range: [-0.5, 0.5],
+  },
+  modebar: {remove: ['pan3d', 'resetCameraDefault3d']},
+  paper_bgcolor: '#ffffff00',
 };
 
-const STABLE_LINE_STYLE = {color: 'blue', width: 3}
-const UNSTABLE_LINE_STYLE = {color: 'blue', width: 3, dash: 'dash'}
-const VERTICAL_INTERSECTION_STYLE = {color:"Red", width: 1}
-const BASE_STABILITY_LINE = {color:"Red", width: 3}
-const STABLE_MARKER = {color:"Green", size: 10}
-const UNSTABLE_MARKER = {color:"Orange", size: 10}
+const STABLE_LINE_STYLE = {
+  color: 'blue',
+  width: 3
+};
+const UNSTABLE_LINE_STYLE = {
+  color: 'blue',
+  width: 3,
+  dash: 'dash'
+};
+const VERTICAL_INTERSECTION_STYLE = {
+  color: 'Red',
+  width: 1
+};
+const BASE_STABILITY_LINE = {
+  color: 'Red',
+  width: 3
+};
+const STABLE_MARKER = {
+  color: 'Green',
+  size: 10
+};
+const UNSTABLE_MARKER = {
+  color: 'Orange',
+  size: 10
+};
 
-let parampSlider = document.getElementById('parampSlider')
+let parampSlider = document.getElementById('parampSlider');
 let parampLabel = document.getElementById('parampSliderLabel');
 let plotlyDiv = document.getElementById('plotlyPitchfork');
 let stabilityDiv = document.getElementById('plotlyStability');
 
 parampLabel.innerHTML = `p = ${parampSlider.value}`;
 
-var upperBranch = []; 
-var lowerBranch = []; 
+var upperBranch = [];
+var lowerBranch = [];
 
 for (var i = 0; i < NUM_POINTS; i++) {
-    var xValue = i * x_LIMSUP / NUM_POINTS
-    upperBranch.push(Math.sqrt(xValue));
-    lowerBranch.push(-Math.sqrt(xValue));
+  var xValue = i * x_LIMSUP / NUM_POINTS;
+  upperBranch.push(Math.sqrt(xValue));
+  lowerBranch.push(-Math.sqrt(xValue));
 }
 
 var traceStableZeroEquilibrium = {
-    x: getLinspace(X_LIMINF, 0, 2),
-    y: Array(NUM_POINTS).fill(0),
-    mode: 'lines',
-    line: STABLE_LINE_STYLE,
-    showlegend: false
+  x: linspace(X_LIMINF, 0, 2),
+  y: Array(NUM_POINTS).fill(0),
+  mode: 'lines',
+  line: STABLE_LINE_STYLE,
+  showlegend: false
 };
 
 var traceUnstableZeroEquilibrium = {
-    x: getLinspace(0, x_LIMSUP, 2),
-    y: Array(NUM_POINTS).fill(0),
-    mode: 'lines',
-    line: UNSTABLE_LINE_STYLE,
-    showlegend: false
+  x: linspace(0, x_LIMSUP, 2),
+  y: Array(NUM_POINTS).fill(0),
+  mode: 'lines',
+  line: UNSTABLE_LINE_STYLE,
+  showlegend: false
 };
 
 var traceUpperBranch = {
-    x: getLinspace(0, x_LIMSUP, NUM_POINTS),
-    y: upperBranch,
-    mode: 'lines',
-    line: STABLE_LINE_STYLE,
-    showlegend: false
-}
+  x: linspace(0, x_LIMSUP, NUM_POINTS),
+  y: upperBranch,
+  mode: 'lines',
+  line: STABLE_LINE_STYLE,
+  showlegend: false
+};
 
 var traceLowerBranch = {
-    x: getLinspace(0, x_LIMSUP, NUM_POINTS),
-    y: lowerBranch,
-    mode: 'lines',
-    line: STABLE_LINE_STYLE,
-    showlegend: false
-}
+  x: linspace(0, x_LIMSUP, NUM_POINTS),
+  y: lowerBranch,
+  mode: 'lines',
+  line: STABLE_LINE_STYLE,
+  showlegend: false
+};
 
 var traceUnstableEqPoints = {
-    x: xUnstablePoints,
-    y: yUnstablePoints,
-    mode: 'markers',
-    marker: UNSTABLE_MARKER,
-    showlegend: false
-}
+  x: xUnstablePoints,
+  y: yUnstablePoints,
+  mode: 'markers',
+  marker: UNSTABLE_MARKER,
+  showlegend: false
+};
 
 var traceVLine = {
-    x: [],
-    y: [],
-    mode: 'lines',
-    line: VERTICAL_INTERSECTION_STYLE,
-    showlegend: false
-} 
+  x: [],
+  y: [],
+  mode: 'lines',
+  line: VERTICAL_INTERSECTION_STYLE,
+  showlegend: false
+};
 
-var traceStableEqPoints = {
-    x: [],
-    y: [],
-    mode: 'markers',
-    marker: STABLE_MARKER,
-    showlegend: false
-}
+var traceStableEqPoints =
+    {x: [], y: [], mode: 'markers', marker: STABLE_MARKER, showlegend: false};
 
-var traceUnstableEqPoints = {
-    x: [],
-    y: [],
-    mode: 'markers',
-    marker: UNSTABLE_MARKER,
-    showlegend: false
-}
+var traceUnstableEqPoints =
+    {x: [], y: [], mode: 'markers', marker: UNSTABLE_MARKER, showlegend: false};
 
 var traceBaseStability = {
-    x: getLinspace(X_LIMINF, x_LIMSUP, NUM_POINTS),
-    y: Array(NUM_POINTS).fill(0),
-    mode: 'lines',
-    line: BASE_STABILITY_LINE,
-    showlegend: false
-}
+  x: linspace(X_LIMINF, x_LIMSUP, NUM_POINTS),
+  y: Array(NUM_POINTS).fill(0),
+  mode: 'lines',
+  line: BASE_STABILITY_LINE,
+  showlegend: false
+};
 
-var traceStabilityStablePoints = {
-    x: [],
-    y: [],
-    mode: 'markers',
-    marker: STABLE_MARKER,
-    showlegend: false
-}
+var traceStabilityStablePoints =
+    {x: [], y: [], mode: 'markers', marker: STABLE_MARKER, showlegend: false};
 
-var traceStabilityUnstablePoints = {
-    x: [],
-    y: [],
-    mode: 'markers',
-    marker: UNSTABLE_MARKER,
-    showlegend: false
-}
+var traceStabilityUnstablePoints =
+    {x: [], y: [], mode: 'markers', marker: UNSTABLE_MARKER, showlegend: false};
 
-var xStablePoints = []
-var yStablePoints = []
-var xUnstablePoints = []
-var yUnstablePoints = []
+var xStablePoints = [];
+var yStablePoints = [];
+var xUnstablePoints = [];
+var yUnstablePoints = [];
 
-var plotData = [traceStableZeroEquilibrium, traceUnstableZeroEquilibrium, traceUpperBranch, 
-                traceLowerBranch, traceVLine, traceStableEqPoints, traceUnstableEqPoints] 
+var plotData = [
+  traceStableZeroEquilibrium, traceUnstableZeroEquilibrium, traceUpperBranch,
+  traceLowerBranch, traceVLine, traceStableEqPoints, traceUnstableEqPoints
+];
 
 Plotly.newPlot(plotlyDiv, plotData, LAYOUT_PITCHFORK);
-Plotly.newPlot(stabilityDiv, [traceBaseStability, traceStabilityStablePoints, traceStabilityUnstablePoints], LAYOUT_STABILITY)
+Plotly.newPlot(
+    stabilityDiv,
+    [
+      traceBaseStability, traceStabilityStablePoints,
+      traceStabilityUnstablePoints
+    ],
+    LAYOUT_STABILITY);
 
 parampSlider.oninput = () => {
-    var param_value = parampSlider.value
-    parampLabel.innerHTML = `p = ${param_value}`;
+  var param_value = parampSlider.value;
+  parampLabel.innerHTML = `p = ${param_value}`;
 
-    if (param_value <= 0) {
-        xStablePoints = [param_value]
-        yStablePoints = [0]
+  if (param_value <= 0) {
+    xStablePoints = [param_value];
+    yStablePoints = [0];
 
-        xUnstablePoints = []
-        yUnstablePoints = []
-    } else {
-        xStablePoints = [param_value, param_value]
-        yStablePoints = [Math.sqrt(param_value), -Math.sqrt(param_value)]
+    xUnstablePoints = [];
+    yUnstablePoints = [];
+  } else {
+    xStablePoints = [param_value, param_value];
+    yStablePoints = [Math.sqrt(param_value), -Math.sqrt(param_value)];
 
-        xUnstablePoints = [param_value]
-        yUnstablePoints = [0]
-    }
-    console.log(parampLabel.innerHTML);
+    xUnstablePoints = [param_value];
+    yUnstablePoints = [0];
+  }
+  console.log(parampLabel.innerHTML);
 
-    var updatedTraces = {x: [[param_value, param_value], xStablePoints, xUnstablePoints],
-                         y: [[-2, 2],yStablePoints, yUnstablePoints]}
-    Plotly.update(plotlyDiv, updatedTraces, {}, [4, 5, 6]);
-    
-    var updatedStabilityData = {x: [yStablePoints, yUnstablePoints],
-                                y: [Array(yStablePoints.length).fill(0), Array(yUnstablePoints.length).fill(0)]}
-    Plotly.update(stabilityDiv, updatedStabilityData, {}, [1, 2])
+  var updatedTraces = {
+    x: [[param_value, param_value], xStablePoints, xUnstablePoints],
+    y: [[-2, 2], yStablePoints, yUnstablePoints]
+  };
+  Plotly.update(plotlyDiv, updatedTraces, {}, [4, 5, 6]);
 
+  var updatedStabilityData = {
+    x: [yStablePoints, yUnstablePoints],
+    y: [
+      Array(yStablePoints.length).fill(0), Array(yUnstablePoints.length).fill(0)
+    ]
+  };
+  Plotly.update(stabilityDiv, updatedStabilityData, {}, [1, 2]);
 };
